6b1baad7dc653987431dc4e45799d82f654426e0
Maxeminator <kil201@mail.ru>
2025-07-08 17:53:55 +0300

    Finished registerHandler

--- commands.go
+++ commands.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/Maxeminator/blog-aggregator/internal/config"
+	"github.com/Maxeminator/blog-aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 type state struct {
-	config *config.Config
+	db  *database.Queries
+	cfg *config.Config
 }
 
 type command struct {
@@ -19,11 +24,31 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) < 1 {
 		return fmt.Errorf("username required")
 	}
-	err := s.config.SetUser(cmd.args[0])
+	err := s.cfg.SetUser(cmd.args[0])
 	if err != nil {
 		return fmt.Errorf("can't set the username: %w", err)
 	}
 	fmt.Printf("username set to: %s\n", cmd.args[0])
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("username required")
+	}
+	name := cmd.args[0]
+	now := time.Now()
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name:      name,
+	})
+	if err != nil {
+		return fmt.Errorf("create user: %w", err)
+	}
+
+	fmt.Printf("user created: id=%d, name=%s\n", user.ID, user.Name)
 	return nil
 }
 
--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/Maxeminator/blog-aggregator/internal/config"
+	"github.com/Maxeminator/blog-aggregator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	dbURL := "postgres://postgres:postgres@localhost:5432/gator?sslmode=disable"
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("can't open db: %v", err)
+	}
+	dbQueries := database.New(db)
+
 	cfg := config.Config{}
-	err := config.Read(&cfg)
+	err = config.Read(&cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	st := &state{config: &cfg}
+
+	st := &state{
+		db:  dbQueries,
+		cfg: &cfg,
+	}
 
 	cmds := &commands{handlers: make(map[string]func(*state, command) error)}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+
 	args := os.Args
 	if len(args) < 2 {
 		fmt.Println("not enough arguments")
